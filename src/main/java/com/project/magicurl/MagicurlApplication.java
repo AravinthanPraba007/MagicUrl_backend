3b40dfebf67e220c51c2d187c6163564ee1c63f1
Aravinthan <aravinthanpsn97@gmail.com>
2021-02-08 20:12:52 +0530

    Initial project setup

---
+++ src/main/java/com/project/magicurl/MagicurlApplication.java
@@ -0,0 +1,13 @@
+package com.project.magicurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicurlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/magicurl/MagicurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.magicurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
